c072aec393df0086e9c24ef147bfecc138c3c897
ayelealazar887-code <ayelealazar887@gmail.com>
2026-07-05 13:57:59 +0300

    initital commit

---
+++ src/config/data.js
@@ -0,0 +1,11 @@
+import pkg from "pg";
+
+const { pool } = pkg;
+
+const pool = new pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.DBPORT,
+});
