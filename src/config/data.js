8d64ca7e37a97e4aa66a28105dd9dccaa2115636
ayelealazar887-code <ayelealazar887@gmail.com>
2026-07-05 14:11:14 +0300

    securing connection

--- src/config/data.js
+++ src/config/data.js
@@ -1,11 +1,20 @@
 import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const { pool } = pkg;
 
 const pool = new pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: process.env.DBPORT,
 });
+
+pool.on("connect", () => {
+  console.log("Connected to the database");
+});
+
+export default pool;
